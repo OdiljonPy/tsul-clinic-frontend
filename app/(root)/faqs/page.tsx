42b7a615a0f93b8fe061e68276393cfea55249e9
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-08-29 20:30:20 +0500

    added:faq page

--- app/(root)/faqs/page.tsx
+++ app/(root)/faqs/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import InnerBanner from "@/components/global/inner-banner";
 import PreFooter from "@/components/shared/PreFooter";
-import FAQsList from "@/components/FAQs/FAQsList";
 import FAQsAccordion from "@/components/FAQs/FAQsAccordion";
 import { Metadata } from "next";
 
@@ -14,13 +13,7 @@ const Page = () => {
   return (
     <>
       <InnerBanner text="FAQS" />
-
       <FAQsAccordion />
-
-      <FAQsList headline="FAQs Style 1" separator />
-
-      <FAQsList headline="FAQs Style 2" columns={2} bgType="white" />
-
       <PreFooter />
     </>
   );
