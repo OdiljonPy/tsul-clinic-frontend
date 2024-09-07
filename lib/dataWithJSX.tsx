d47607881078cdf991666e3fbd146a5ca87072f5
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-09-07 12:52:04 +0500

    added content for home banner statistict

--- lib/dataWithJSX.tsx
+++ lib/dataWithJSX.tsx
@@ -31,9 +31,9 @@ import {
 export const BannerIconBoxes: BannerIconBoxesProps[] = [
   {
     id: 1,
-    headline: "20+ Years Of Experience",
+    headline: "15+ Yillik tajriba",
     content:
-      "We are in the law industry for more than 20 years with more than 98% of client satisfaction",
+      "Biz 15 yildan ortiq vaqt mobaynida mijozlarimizga yuridik xizmat ko'rsatib kelmoqdamiz",
     iconName: (
       <User
         size="60"
@@ -44,9 +44,9 @@ export const BannerIconBoxes: BannerIconBoxesProps[] = [
   },
   {
     id: 2,
-    headline: "Qualified And Experienced Attorneys",
+    headline: "Malakali va tajribali yuristlar",
     content:
-      "Our team is the best in the business. We are team of attorneys which are passionate about their jobs.",
+      "Bizning jamoamiz o'z sohasining yetuk mutaxassislaridan tashkil topgan",
     iconName: (
       <BriefcaseBusiness
         size="60"
@@ -57,9 +57,9 @@ export const BannerIconBoxes: BannerIconBoxesProps[] = [
   },
   {
     id: 3,
-    headline: "Awards And Achievements",
+    headline: "Mukofotlar va yutuqlar",
     content:
-      "We at lawyero earned the best law firm award in 2017 with a success ratio of 99%.",
+      "Biz yuristlar 2023 yilda 95% muvaffaqiyat  bilan eng yaxshi yuristlar mukofotini qo'lga kiritdik.",
     iconName: (
       <Trophy
         size="60"
