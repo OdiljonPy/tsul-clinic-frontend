78ecbc35ce3575637a53f61cdc951e95fcee4be3
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-09-09 20:41:05 +0500

    added:faq into hone

--- components/home/FAQs.tsx
+++ components/home/FAQs.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import PrimaryHeadline from "../global/primary-headline";
 import { FAQData } from "@/lib/data";
 import FAQItem from "../shared/FAQItem";
+import useFAQStore from "@/store/faq/faq";
+import { useEffect } from "react";
+import Loading from "@/app/(root)/loading";
 
 const FAQs = () => {
+  const { faq, fetchFAQ, loading } = useFAQStore();
+
+  useEffect(() => {
+    if (faq.length === 0) fetchFAQ();
+  }, [fetchFAQ]);
+
+  if (loading) return <Loading />;
+
   return (
     <div className="bg-grey pb-11 pt-20">
       <div className="container">
         <PrimaryHeadline text="Frequently Asked Questions" />
         <div className="flex flex-wrap pt-3 ">
-          {FAQData.map((item) => (
+          {faq?.map((item) => (
             <FAQItem
               key={`faq${item.id}`}
               question={item.question}
---
+++ store/faq/faq.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+import API from "@/lib/axios";
+import { ApiResponse } from "@/types/api-response";
+import { IFaq } from "@/types/home/faq";
+
+type FaqStoreType = {
+  faq: IFaq[];
+  loading: boolean;
+  error: boolean;
+  fetchFAQ: () => Promise<void>;
+};
+
+const useFAQStore = create<FaqStoreType>((set) => ({
+  faq: [],
+  loading: false,
+  error: false,
+  fetchFAQ: async () => {
+    set({ loading: true, error: false });
+    try {
+      const res = await API.get<ApiResponse<IFaq>>("/faqs");
+      if (res.data.ok) set({ faq: res.data.response });
+    } catch {
+      set({ error: true });
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
+
+export default useFAQStore;
