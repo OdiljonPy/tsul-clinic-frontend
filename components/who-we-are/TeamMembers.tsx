5b7c1872ff66ebc04b2acb17b3b602db8f7a503b
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-09-09 20:10:41 +0500

    added:team member into about us

--- components/who-we-are/TeamMembers.tsx
+++ components/who-we-are/TeamMembers.tsx
@@ -1,12 +1,26 @@
+"use client";
 import PrimaryHeadline from "../global/primary-headline";
 import { TeamMembersCarousel } from "../shared/TeamMembersCarousel";
+import useTeamStore from "@/store/home/team";
+import React, { useEffect } from "react";
+import Loading from "@/app/(root)/loading";
 
 const TeamMembers = () => {
+  const { team, loading, fetchTeam } = useTeamStore();
+
+  useEffect(() => {
+    if (team.length === 0) fetchTeam();
+  }, [fetchTeam]);
+
+  if (loading) return <Loading />;
   return (
     <div className="bg-background">
       <div className="container pb-40 pt-20 lg:pb-52 lg:pt-28">
-        <PrimaryHeadline text="Bizning jamoa" additionalClass="secondary-headline" />
-        <TeamMembersCarousel textColor="text-white" />
+        <PrimaryHeadline
+          text="Bizning jamoa"
+          additionalClass="secondary-headline"
+        />
+        <TeamMembersCarousel team={team} textColor="text-white" />
       </div>
     </div>
   );
