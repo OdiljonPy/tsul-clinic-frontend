06a983935a70e759902fb0b7d90a9b7bed8177d3
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-09-11 20:22:58 +0500

    added:transition into contact page

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -2,9 +2,9 @@ import InnerBanner from "@/components/global/inner-banner";
 import { ContactForm } from "@/components/global/ContactForm";
 import PrimaryHeadline from "@/components/global/primary-headline";
 import Iframe from "@/components/shared/Iframe";
-import { MapPin, PhoneCallIcon } from "lucide-react";
 import { Metadata } from "next";
 import ContactInfo from "@/components/contact/ContactInfo";
+import { getTranslation } from "@/i18n";
 
 export const metadata: Metadata = {
   title: "Contact Us - Lawyero",
@@ -12,9 +12,10 @@ export const metadata: Metadata = {
 };
 
 const page = () => {
+  const { t } = getTranslation();
   return (
     <>
-      <InnerBanner text="Contact Page" />
+      <InnerBanner text={t("contact")} />
       <div className="w-full">
         <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2712.0035797925284!2d69.27547587552999!3d41.31187630067642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b0013034b7f%3A0xe1f8fb9b87290692!2sYuridik%20Instituti!5e1!3m2!1sen!2s!4v1725692982751!5m2!1sen!2s" />
       </div>
@@ -24,14 +25,12 @@ const page = () => {
           <div className="flex flex-wrap lg:flex-nowrap">
             <div className="relative basis-full pb-20 pr-0 pt-16 before:z-[-1] lg:basis-2/3 lg:pr-12 lg:pt-24">
               <PrimaryHeadline
-                text="Aloqaga chiqish uchun "
+                text={t("for_contact")}
                 additionalClass="text-3xl primary-headline-left"
                 headlineType="h3"
               />
               <p className="mb-5 text-base leading-7 text-[#333]">
-                Maslahat olish uchun bizga qo'ng'iroq qiling yoki quyidagi
-                formani to'ldiring. Bizning vakillarimizdan biri sizga 24 soat
-                ichida javob beradi.
+                {t("for_contact_desc")}
               </p>
               <ContactForm />
             </div>
