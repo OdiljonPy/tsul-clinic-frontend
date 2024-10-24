8efd81ea47ea97edc4b3c7412e8609eb55660336
Valisher Botirov <valisherbotirov1218@gmail.com>
2024-10-24 15:38:26 +0500

    change jivo chat token

--- components/JivoChat.tsx
+++ components/JivoChat.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 const JivoChat = () => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "//code.jivosite.com/widget/b5BYeZYhWX";
+    script.src = "//code.jivosite.com/widget/J1shxFqHcJ";
     script.async = true;
     document.body.appendChild(script);
     return () => {
